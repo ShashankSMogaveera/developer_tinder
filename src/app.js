3ba7c4fea206d294e01d6c456abdcd4b8fb1b8a1
shashank-epam <shashankshekar_mogaveera@epam.com>
2024-10-23 17:11:38 +0530

    chores: created a dummy server

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ const express= require('express');
 
 const app= express();
 
+app.use('/login',(_req,res)=>{
+    res.send('hii fron login')
+})
 app.use('/',(_req,res)=>{
     res.send('hii from /')
 })
 
-app.listen(6767);
+app.listen(6767,(_req,_res)=>{
+    console.log('server running ')
+});
