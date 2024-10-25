5670dfeb6cf82b17dd8aa712e95d9a5d8df31264
shashank-epam <shashankshekar_mogaveera@epam.com>
2024-10-25 22:32:33 +0530

    chores: practiced different http methods

--- src/app.js
+++ src/app.js
@@ -2,12 +2,32 @@ const express= require('express');
 
 const app= express();
 
-app.use('/login',(_req,res)=>{
-    res.send('hii fron login')
+app.get('/user',(_req,res, next)=>{
+    console.log("middleware 1")
+    next();
+},(_req,res,next)=>{
+    console.log('middleware 2')
+    next()
+},(req,res)=>{
+    res.send('route handler last')
 })
-app.use('/',(_req,res)=>{
-    res.send('hii from /')
+
+app.get('/user',[(_req,res, next)=>{
+    console.log("middleware 1")
+    next();
+}],(_req,res,next)=>{
+    console.log('middleware 2')
+    next()
+},(req,res)=>{
+    res.send('route handler last')
 })
+
+app.post('/user',(_req,res)=>{
+    res.send("User data received");
+})
+// app.use('/',(_req,res)=>{
+//     res.send('hii from /')
+// })
 
 app.listen(6767,(_req,_res)=>{
     console.log('server running ')
