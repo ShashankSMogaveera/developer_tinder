07eafed99f55161932f8cf60a7484d9483ba12d4
shashank-epam <shashankshekar_mogaveera@epam.com>
2024-10-27 19:35:55 +0530

    feature: added schema validations

--- src/app.js
+++ src/app.js
@@ -6,6 +6,60 @@ const app= express();
 
 app.use(express.json())
 
+app.get('/user',async (req,res)=>{
+    const userEmail=req.body.email;
+    try{
+        const user= await User.findOne({email:userEmail});
+        console.log(user)
+        if(user){
+            res.send(user)
+        }else{
+            res.status(404).send('user not found');
+        }
+    }catch{
+        res.status(500).send('something went wrong')
+    }
+})
+
+app.get('/feed',async (_req,res)=>{
+    try{
+        const users= await User.find({});
+        res.send(users);
+        
+    }catch{
+        res.send('something went wrong');
+    }
+})
+
+app.delete('/user',async (req,res)=>{
+    try{
+        await User.findOneAndDelete({email:req.body.email})
+        res.send('user deleted successfully');
+    }catch{
+        res.send('something went wrong');
+    }
+
+})
+
+app.put('/user',async (req,res)=>{
+    try{
+        await User.findByIdAndUpdate(req.body.id,req.body, {runValidators: true});
+        res.send('update successfull');
+    }catch(err){
+        res.send('something went wrong'+err.message)
+    }
+})
+
+// app.put('/user',async (req,res)=>{
+//     try{
+
+//         await User.findOneAndUpdate({'email':req.body.email},req.body);
+//         res.send('update successfull');
+//     }catch(err){
+//         res.send(`something went wrong ${err.message}`)
+//     }
+// })
+
 app.post('/signup',async (req,res)=>{
     // console.log(req.body);
     const userData=new User(req.body)
@@ -13,7 +67,7 @@ app.post('/signup',async (req,res)=>{
         await userData.save();
         res.send('user signedup successfully')
     }catch(err){
-        res.status(400).send('user signup unsuccessfull')
+        res.status(400).send('user signup unsuccessfull '+err.message)
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,70 @@ const Mongoose= require('mongoose');
 
 const userSchema=new Mongoose.Schema({
     firstName : {
-        type: String
+        type: String,
+        require: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 25
     },
     lastName : {
-        type: String
+        type: String,
+        require: true,
+        trim: true,
+        minLength: 1,
+        maxLength: 25
     },
     email: {
-        type: String
+        type: String,
+        require: true,
+        unique: true,
+        trim: true,
+        validate(value){
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+            if(! emailRegex.test(value)){
+                throw new Error('incorrect Email pattern ')
+            }
+        },
+        minLength: 13
     },
-    password: {
-        type: String
+    password: { 
+        type: String,
+        minLength: 6,
+        maxLength: 30,
+        trim: true,
+        validate(value){
+            const passwordRegex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[.@$*\\-_])[a-zA-Z0-9.@$*\\-_]{6,30}$/;
+            if(!passwordRegex.test(value)){
+                throw new Error('incorrect password pattern');
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 12
     },
     gender: {
-        type: String
+        type: String,
+        trim: true,
+        validate(value){
+            if(!['male','female'].includes(value.toLowerCase())){
+                throw new Error('invalid gender')
+            }
+        }
+    },
+    skills: {
+        type:[String],
+    },
+    profilePhoto: {
+        type:String,
+        default: "https://play-lh.googleusercontent.com/EotxkWC4dXajaesh2iVgdIB5-o6pINoas_k-z7nVjRGSu4k9QZwMZIcRNXyUWGn3rg"
+    },
+    description: {
+        type:String,
+        default: 'not available'
     }
+},{
+    timestamps: true
 })
 
 const User=Mongoose.model("User", userSchema);
